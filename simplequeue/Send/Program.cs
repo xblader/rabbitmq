7ef153b031d6387efa7e393bc5214e138a811c48
victor pinheiro <xblader@gmail.com>
2018-08-26 11:55:32 -0300

    ok

---
+++ simplequeue/Send/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                
+                while (true)
+                {
+                    i++;
+                    string messagesent = $" [x] Sent {message} - {i}";
+                    var body = Encoding.UTF8.GetBytes(messagesent);
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(messagesent);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    //Console.ReadLine();
+                }
+            }            
+        }
+    }
+}
